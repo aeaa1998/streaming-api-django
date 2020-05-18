e346db7f114b58751bedf49137269d17c982f699
andre <xplay568@gmail.com>
2020-05-17 23:11:14 -0600

    Revert "fix on  relation"
    This reverts commit edf1872b76b2bdb388858dc203afcbf227b89c58.

--- playlists/models.py
+++ playlists/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from tracks.models import Track
+from profiles.models import Profile
 
 # Create your models here.
 class Playlist(models.Model):
 	name = models.CharField(max_length=50)
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="playlists")
-	tracks = models.ManyToManyField(Track, blank=True, related_name="playlist")
 	created_at = models.DateTimeField(auto_now_add=True, blank=True)
 	updated_at = models.DateTimeField(auto_now_add=True, blank=True)
 	class Meta:
